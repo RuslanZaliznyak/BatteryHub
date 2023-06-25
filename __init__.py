893a4ee7e36836fa69e84865c96b7326575652cd
Ruslan Sydir <rsydir@gmail.com>
2023-06-25 22:19:25 +0200

    Added WSGI - Waitress

--- __init__.py
+++ __init__.py
@@ -1,31 +1,25 @@
 import logging
 from flask import Flask
+from waitress import serve
 from app.config import Config
 from app.battery_manager import bp as battery_manager_bp
-from waitress import serve
 
 
 def create_app(config_class=Config):
+    # Flask App Initialization
     app = Flask(__name__)
-
-    app.logger.setLevel(logging.DEBUG)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-
-    app.logger.addHandler(console_handler)
-
-    waitress_logger = logging.getLogger('waitress')
-    waitress_logger.setLevel(logging.DEBUG)
-    waitress_logger.addHandler(logging.StreamHandler())
-
     app.config.from_object(config_class)
 
     app.register_blueprint(battery_manager_bp)
+
+    # Logs Initialization
+    app.logger.setLevel(logging.DEBUG)
 
     return app
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
     app = create_app()
-    app.run(port=5001)
+    serve(app, host='127.0.0.1', port=5001)
